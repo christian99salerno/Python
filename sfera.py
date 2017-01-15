5197051e185d0a969e29f9a9214f86c6425898d0
christian99salerno <christian.salerno05@gmail.com>
2017-01-15 12:58:57 +0100

    Aggiunto programma che calcola l'area e il volume di una sfera

---
+++ sfera.py
@@ -0,0 +1,7 @@
+import math
+r = input("Digita il valore del raggio di una sfera ")
+float(r)
+superficie = 4 * math.pi * r * r
+volume = (4/3) * math.pi * r * r * r
+print "La superficie della sfera e'", superficie
+print "Il suo volume e'", volume
