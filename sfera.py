4fcc646dc95567ae5e0ab715aa813910564bd78c
christian99salerno <christian.salerno05@gmail.com>
2017-01-16 14:59:50 +0100

    Volume aggiustato

--- sfera.py
+++ sfera.py
@@ -1,6 +1,6 @@
 import math
 r = input("Digita il valore del raggio di una sfera ")
 superficie = 4 * math.pi * r * r
-volume = (4/3) * math.pi * r * r * r
+volume = (4.0/3.0) * math.pi * r * r * r
 print "La superficie della sfera e'", superficie
 print "Il suo volume e'", volume
